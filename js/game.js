8437daa38f4bae7433513d275d7fc6d486116d8f
Wang Wenhao <wangwenhao@live.cn>
2024-11-22 14:28:32 +0800

    game scene manage

--- js/game.js
+++ js/game.js
@@ -2,9 +2,41 @@
 (function(){
   var game = window.colorQuestGame = window.colorQuestGame || {};
 
+  game.flow = {
+    startOver: function() {
+      game.startScene.hide();
+      game.summaryScene.hide();
+      game.gameoverScene.hide();
+      game.gameScene.hide();
+      game.startScene.show();
+    },
+    gameWin: function() {
+      game.gameScene.hide();
+      game.summaryScene.show();
+    },
+    gameOver: function() {
+      game.startScene.show();
+      game.gameScene.hide();
+      game.gameoverScene.show();
+    },
+    nextLevel: function() {
+      game.startScene.hide();
+      game.summaryScene.hide();
+      game.gameScene.show();
+    },
+    finishLevel: function() {
+      game.gameScene.hide();
+      game.summaryScene.show();
+    }
+  }
+
   // Entry Point
   var init = function() {
-  	console.log("Welcome to Color Quest Game.");
+    console.log("Welcome to Color Quest Game.");
+    game.startScene.handleInput();
+    game.summaryScene.handleInput();
+    game.gameoverScene.handleInput();
+    game.gameScene.handleInput();
   }
 
   // window.onload = init; // use window.onload (or when DOM ready) if the script is not placed at the end of the HTML file.
